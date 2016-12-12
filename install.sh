947fc1356dcd9b9a127d0af5624b759b1bc5b647
Will Zhang <young.zhana@gmail.com>
2016-12-12 11:00:30 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,4 @@
 
 ln -s ~/Dropbox/macSetting/bash_profile ~/.bash_profile
 ln -s ~/Dropbox/macSetting/gitconfig ~/.gitconfig
-ln -s ~/Dropbox/macSetting/.vi
-ln -s ~/Dropbox/macSetting/.vimrc ~/.vimrc
 ln -s ~/Dropbox/macSetting/.irbrc ~/.irbrc
-
-git submodule init
-git submodule update
